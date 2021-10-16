b6738a44e9a6c4c8d409367302319fd5df8e2eff
jimmiebtlr <jimmiebtlr@gmail.com>
2021-10-16 16:39:38 +0000

    Add config for cluster

---
+++ nailing_zero_downtime_deployments_in_k8s/infra/main.tf
@@ -0,0 +1,17 @@
+variable "project" {}
+
+provider "google" {
+  project = var.project
+  region  = "us-central1"
+}
+
+resource "google_container_cluster" "no_downtime" {
+  name               = "no-downtime-deploys"
+  location           = "us-central1-a"
+  initial_node_count = 1
+}
+
+
+output "kubectl_cfg_cmd" {
+  value = "\ngcloud container clusters get-credentials --zone=${google_container_cluster.no_downtime.location} ${google_container_cluster.no_downtime.name}\n"
+}
